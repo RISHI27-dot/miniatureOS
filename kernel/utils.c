95fb8d78e9b43b5be15d6ede85407a46300beb80
Rishikesh Donadkar <rishikeshdonadkar@gmail.com>
2023-02-10 10:39:53 +0530

    utils:: Add utils for string and memory operations.
    For now add the utils that are required to implement scroll on display.
    Later(Maybe after memory allocation is in place.) this utils will be shifted to libc.
    
    Signed-off-by: Rishikesh Donadkar <rishikeshdonadkar@gmail.com>

---
+++ kernel/utils.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "utils.h"
+
+void kmemcpy(uint8_t *source, char *dest, int nbytes) {
+    int i;
+    for (i = 0; i < nbytes; i++) {
+        *(dest + i) = *(source + i);
+    }
+}
+
+void kmemset(uint8_t *dest, uint8_t val, uint32_t len) {
+    uint8_t *temp = (uint8_t *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
+}
+
+void int_to_ascii(int n, char str[]) {
+    int i, sign;
+    if ((sign = n) < 0) n = -n;
+    i = 0;
+    do {
+        str[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
+
+    if (sign < 0) str[i++] = '-';
+    str[i] = '\0';
+
+	/* reverse the string to get correct ascii representation
+	 * of the int.
+     */
+    reverse(str);
+}
+
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
+}
+
---
+++ kernel/utils.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stddef.h>
+
+void memory_set(uint8_t *dest, uint8_t val, uint32_t len);
+void kmemcpy(uint8_t *source, char *dest, int nbytes);
+void int_to_ascii(int n, char str[]); 
+void reverse(char s[]);
+int strlen(char s[]);
+
